6e3a83f3107ce75edae3f7b0cd42b9a865ef26ff
GOULASHSUP <simjob01@easv365.dk>
2023-04-19 18:43:34 +0200

    commit at 18:43
    save

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HeroSection from "./components/HeroSection.vue";
 import Projects from "./components/Projects.vue";
 import AboutMe from "./components/AboutMe.vue";
 import Contact from "./components/Contact.vue";
+
 </script>
 
 <template>
