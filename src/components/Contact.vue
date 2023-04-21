f099f3f9da203a7fc1d044a94d032ffa09fdf306
GOULASHSUP <simjob01@easv365.dk>
2023-04-21 08:11:37 +0200

    contact links updated
    links ease-in duration added

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,20 +2,19 @@
     <section class="bg-background2" id="contact-section">
         <div class="lg:px-44 py-7">
             <h2 class="text-green font-bold text-4xl justify-center flex pb-5 animate-pulse">Contact me</h2>
-            <p class="text-lightgray text-2xl justify-center flex pb-2 hover:text-purple ease-in"><a href="mailto:jobbagy.simon@googlemail.com" target="_blank" rel="noreferrer">
+            <p class="text-lightgray text-2xl justify-center flex pb-2 hover:text-purple ease-in duration-150"><a href="mailto:jobbagy.simon@googlemail.com" target="_blank" rel="noreferrer">
                 jobbagy.simon@gmail.com
             </a></p>
-            <p class="text-lightgray text-2xl justify-center flex pb-2 hover:text-purple ease-in"><a href="tel:+1 (555) 234-5678"> 
+            <p class="text-lightgray text-2xl justify-center flex pb-2 hover:text-purple ease-in duration-150"><a href="tel:+1 (555) 234-5678"> 
                 +45 91 83 77 25
             </a></p>
-            <p class="text-lightgray text-2xl justify-center flex pb-2 hover:text-purple ease-in"><a href="https://www.linkedin.com/in/simon-jobb%C3%A1gy/">LinkeIn/in/simon-jobbágy/</a></p>
+            <p class="text-lightgray text-2xl justify-center flex pb-2 hover:text-purple ease-in duration-150"><a href="https://www.linkedin.com/in/simon-jobb%C3%A1gy/" target="_blank">LinkeIn/in/simon-jobbágy/</a></p>
             <p class="justify-center text-purple flex pt-6">All rights reserved Simon Jobbágy 2023</p>
         </div>
     </section>
 </template>
 
 <script setup>
-
 </script>
 
 <style lang="scss" scoped>
